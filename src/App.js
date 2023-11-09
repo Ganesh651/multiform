69de15f31b00effe89d3b3c52daa46ab54d2d76e
Ganesh651 <ganireddy651@gmail.com>
2023-11-09 14:10:16 +0530

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import MultiStepForm from './component/MultiStepForm'
+import FormContext from './context/FormContext'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [stepNum, setStepNum] = useState(1)
+  const [togglePlan, setTogglePlan] = useState(false)
+  const [choosePlan, setChoosePlan] = useState(false)
+  const [chooseAddOns, setChooseAddOns] = useState(false)
+  const [summery, setSummery] = useState([])
+
+  const nextStep = () => {
+    setStepNum(stepNum + 1)
+  }
+
+  const previousStep = () => {
+    setStepNum(stepNum - 1)
+  }
+
+  const changeTogglePlan = () => {
+    setTogglePlan(!togglePlan)
+  }
+
+  const changeChoosePlan = () => {
+    setChoosePlan(!choosePlan)
+  }
+
+  const changeChooseAddOns = () => {
+    setChooseAddOns(!chooseAddOns)
+  }
+
+  const addToSummery = (price, service, plan, PlanPrice) => {
+    const newPlan = {
+      id: uuidv4(),
+      price,
+      service,
+      plan,
+      PlanPrice,
+    }
+    setSummery(newPlan)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <FormContext.Provider
+      value={{
+        stepNum,
+        nextStep,
+        previousStep,
+        togglePlan,
+        changeTogglePlan,
+        choosePlan,
+        changeChoosePlan,
+        chooseAddOns,
+        changeChooseAddOns,
+        summery,
+        addToSummery,
+      }}
+    >
+      <MultiStepForm />
+    </FormContext.Provider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/context/FormContext/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FormContext = React.createContext({
+  stepNum: 1,
+  nextStep: () => { },
+  previousStep: () => { },
+  togglePlan: 'Monthly',
+  changeTogglePlan: () => { },
+  choosePlan: false,
+  changeChoosePlan: () => { },
+  chooseAddOns: false,
+  changeChooseAddOns: () => { },
+  summery: [],
+  addToSummery: () => { },
+})
+
+export default FormContext
